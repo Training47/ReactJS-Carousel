48da88d58a37c672f21dc88d853340e10acb8f27
Lisa Pena <lisapena13@gmail.com>
2020-09-17 11:48:08 -0500

    Added images

--- src/App.js
+++ src/App.js
@@ -11,23 +11,37 @@ function App() {
   return (
 <div style={styles}>
 <Carousel>
+
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
-<p className="legend">Hong Kong</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTH88KNjoONLpFrb9GwsNq3UCp9a557NIlt5Q&usqp=CAU" alt="Dahlia"/>
+<p className="legend">Fall Bunch</p>
 </div>
+
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
-<p className="legend">Singapore</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSLvcN34sJpkdquJm1labISbPaveDehCl21seZdgWj0ikZIaGIjB_JDbcOdw&usqp=CAc" alt="Asiatic Lily"/>
+<p className="legend">Dahlia</p>
 </div>
+
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
-<p className="legend">Japan</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRX0P1cveuJ6GzPYQYr-GYTbOPD0gCtZHwing&usqp=CAU" alt="Fall Flower Bunch"/>
+<p className="legend">Fall Flower Bunch</p>
 </div>
+
+
 <div>
-<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
-<p className="legend">Las Vegas</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlowYoosY4k8ghhMhtVhxOJkMJauNQAes7a5X0CnEPSxk1ZRl34YESlq1uuUw&usqp=CAc" alt="Blue Bunch"/>
+<p className="legend">Fall Flower Bunch</p>
 </div>
+
+
+<div>
+<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRr6Kaok1qBIUZ3WobEG_yqF7_KybceQuyzeg&usqp=CAUalt" alt="Fall Flower"/>
+<p className="legend">Fall Flower Bunch</p>
+</div>
+
 </Carousel>
+
+
 </div>
   );
 }
