2dd2f06ffdf960b7eb3d6587f7eb550efbbe8cbf
Lisa Pena <lisapena13@gmail.com>
2020-09-17 12:19:41 -0500

    Updates

---
+++ precache-manifest.9a8b93022e8cb98ed34fce101e686bab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be220c3af6a9f9e28c22a9c2da5ba50e",
+    "url": "/ReactJS-Carousel/index.html"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/ReactJS-Carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "58be5553e02ed0d1638a",
+    "url": "/ReactJS-Carousel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91ef500d565c1ca9af0b",
+    "url": "/ReactJS-Carousel/static/js/2.93e32125.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactJS-Carousel/static/js/2.93e32125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58be5553e02ed0d1638a",
+    "url": "/ReactJS-Carousel/static/js/main.b7ee9f51.chunk.js"
+  },
+  {
+    "revision": "77bc0f84a3b39ae85af4",
+    "url": "/ReactJS-Carousel/static/js/runtime-main.4c8ca77f.js"
+  }
+]);
