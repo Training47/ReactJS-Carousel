a03f2923aa5bcbb57e6ba1a66ba1a30a8e755eab
Lisa Pena <lisapena13@gmail.com>
2020-09-19 12:14:52 -0500

    Changed pictures titles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 let styles = {
     margin: 'auto',
-    width: '500px'
+    width: '500px',
+        
   };
   
 function App() {
@@ -14,29 +15,29 @@ function App() {
 
 <div>
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTH88KNjoONLpFrb9GwsNq3UCp9a557NIlt5Q&usqp=CAU" alt="Dahlia"/>
-<p className="legend">Fall Bunch</p>
+<p className="legend">Rose Dahlia</p>
 </div>
 
 <div>
 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSLvcN34sJpkdquJm1labISbPaveDehCl21seZdgWj0ikZIaGIjB_JDbcOdw&usqp=CAc" alt="Asiatic Lily"/>
-<p className="legend">Dahlia</p>
+<p className="legend">Asiatic Lily</p>
 </div>
 
 <div>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRX0P1cveuJ6GzPYQYr-GYTbOPD0gCtZHwing&usqp=CAU" alt="Fall Flower Bunch"/>
-<p className="legend">Fall Flower Bunch</p>
-</div>
-
-
-<div>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlowYoosY4k8ghhMhtVhxOJkMJauNQAes7a5X0CnEPSxk1ZRl34YESlq1uuUw&usqp=CAc" alt="Blue Bunch"/>
-<p className="legend">Fall Flower Bunch</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRX0P1cveuJ6GzPYQYr-GYTbOPD0gCtZHwing&usqp=CAU" alt="Fall Mums"/>
+<p className="legend">Fall Mums</p>
 </div>
 
 
 <div>
-<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRr6Kaok1qBIUZ3WobEG_yqF7_KybceQuyzeg&usqp=CAUalt" alt="Fall Flower"/>
-<p className="legend">Fall Flower Bunch</p>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTlowYoosY4k8ghhMhtVhxOJkMJauNQAes7a5X0CnEPSxk1ZRl34YESlq1uuUw&usqp=CAc" alt="Pansies"/>
+<p className="legend">Pansy Bunch</p>
+</div>
+
+
+<div>
+<img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRekYKcIkq993C9zOUYdgD9YQMXc5yEzhrrRQ&usqp=CAU" alt="Dahlia Red"/>
+<p className="legend">Dahlia Red</p>
 </div>
 
 </Carousel>
